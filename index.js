91556eea141b1f52cacffa2ab50dfbdadd66ba40
Hans-Juergen Grauer <Hans-Juergen.Grauer@dci-student.org>
2025-11-03 01:03:28 +0100

    Code aufgeraeumt

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/************************************************** User defined Vars *******************************************/
+/* ############################################## Globals ######################################### */
 // Farben
-const palette = [
-  "#000000", //Grundfarbe! Nicht ändern.
+const PALETTE = [
+  "#000000", // Index 0 ist Farbe für tote Zellen.
+  "#ff0000",
+  "#00ff00",
+  "#ffff00",
+  "#0000ff",
+  "#ff00ff",
+  "#ffffff",
   "#00ffff",
   "#800000",
   "#008000",
@@ -11,31 +17,19 @@ const palette = [
   "#008080",
   "#c0c0c0",
   "#808080",
-  "#ff0000",
-  "#00ff00",
-  "#ffff00",
-  "#0000ff",
-  "#ff00ff",
-  "#ffffff",
 ];
 
 // Tabellenformat
-// const zeilen = 45;
-// const spalten = 110;
-const width = "12px";
-const height = width;
+const ZEILEN = 45;
+const SPALTEN = 110;
+const CELL_WIDTH = "12px";
+const CELL_HEIGHT = CELL_WIDTH;
 
-// Farbwechsel
-const colorCycling = true;
+/* ############################################## Main ######################################### */
 
-// Speed
-const intervall = 1;
-
-//
 const Game = {
-  zeilen: 45,
-  spalten: 110,
-  defaultColor: 10, // Defaultfarbe für die Zellen
+  zeilen: ZEILEN,
+  spalten: SPALTEN,
   rounds: 0,
   matrix: [],
 
@@ -49,8 +43,7 @@ const Game = {
   transformMatrix() {
     const zeilen = this.zeilen;
     const spalten = this.spalten;
-    const defaultColor = this.defaultColor;
-    const colors = palette.length - 1;
+    const colors = PALETTE.length - 1;
     const old_matrix = this.matrix;
     const new_matrix = Array(zeilen)
       .fill(0)
@@ -62,7 +55,7 @@ const Game = {
       for (let dy = -1; dy <= 1; dy++) {
         const yy = (y + dy + zeilen) % zeilen;
         for (let dx = -1; dx <= 1; dx++) {
-          if (dy === 0 && dx === 0) continue;
+          if (dy === 0 && dx === 0) continue; // sich selbst nicht mitzählen.
           const xx = (x + dx + spalten) % spalten;
           if (old_matrix[yy][xx] !== 0) n++;
         }
@@ -84,14 +77,12 @@ const Game = {
         // Für´s Erste nur klassische Regeln
         if (old_matrix[y][x] === 0) {
           //Zelle tot
-          if (neighbours === 3) new_matrix[y][x] = defaultColor; // Bei exakt 3 lebenden Nachbarn befruchten
+          if (neighbours === 3) new_matrix[y][x] = 1; // Bei exakt 3 lebenden Nachbarn befruchten
         } else {
           // Zelle lebt
           if (neighbours === 2 || neighbours === 3)
-            if (colorCycling === true)
-              // Darf weiterleben
-              new_matrix[y][x] = cycleCol(old_matrix[y][x]);
-            else new_matrix[y][x] = defaultColor;
+            // Zelle updaten
+            new_matrix[y][x] = cycleCol(old_matrix[y][x]);
         }
       }
     }
@@ -100,7 +91,52 @@ const Game = {
   },
 };
 
-/********************************************************* UI *********************************************/
+/* ############################################## Setup and run ######################################### */
+
+// R-pentomino in Matrix poppen
+function r_Pentomino() {
+  const x0 = SPALTEN >>> 1;
+  const y0 = ZEILEN >>> 1;
+  const m = Game.matrix;
+  m[y0 - 1][x0 + 1] = 1;
+  m[y0 - 2][x0 + 1] = 1;
+  m[y0 - 2][x0 + 0] = 1;
+  m[y0 - 3][x0 + 1] = 1;
+  m[y0 - 3][x0 + 2] = 1;
+}
+
+const Simulation = {
+  interval: 0,
+  loopPtr: null,
+  inProgress: false,
+
+  start() {
+    this.loopPtr = setInterval(this.loop, this.interval);
+    this.inProgress = true;
+  },
+
+  stop() {
+    clearInterval(this.loopPtr);
+    this.inProgress = false;
+  },
+
+  loop() {
+    Game.transformMatrix();
+    viewport_container(Game.matrix);
+    counter.count();
+  },
+
+  reset() {
+    this.stop();
+    Game.new();
+    r_Pentomino();
+    viewport_container(Game.matrix);
+  },
+};
+
+Simulation.reset();
+
+/* ###################################################### UI ################################################## */
 
 // Viewport für die Seite bauen
 function viewport_container(matrix) {
@@ -114,9 +150,9 @@ function viewport_container(matrix) {
     const row = document.createElement("tr");
     rowData.forEach((value, x) => {
       const cell = document.createElement("td");
-      cell.style.backgroundColor = palette[value];
-      cell.style.width = width;
-      cell.style.height = height;
+      cell.style.backgroundColor = PALETTE[value];
+      cell.style.width = CELL_WIDTH;
+      cell.style.height = CELL_HEIGHT;
 
       cell.dataset.x = x;
       cell.dataset.y = y;
@@ -145,8 +181,8 @@ function viewport_container_editor(matrix) {
     const x = parseInt(cell.dataset.x);
     const y = parseInt(cell.dataset.y);
 
-    matrix[y][x] = (matrix[y][x] + 1) % palette.length;
-    cell.style.backgroundColor = palette[matrix[y][x]];
+    matrix[y][x] = (matrix[y][x] + 1) % PALETTE.length;
+    cell.style.backgroundColor = PALETTE[matrix[y][x]];
   };
 
   let mouseDown = false;
@@ -159,70 +195,55 @@ function viewport_container_editor(matrix) {
   });
 }
 
-/************************************************* Setup and run ******************************** */
+// Rundenzähler
+class Counter {
+  value = 0;
+  name = "counter";
+  text = "Rounds: ";
 
-// R-pentomino bauen
-function r_Pentomino() {
-  const x0 = Game.spalten >>> 1;
-  const y0 = Game.zeilen >>> 1;
-  Game.matrix[y0 - 1][x0 + 1] = Game.defaultColor;
-  Game.matrix[y0 - 2][x0 + 1] = Game.defaultColor;
-  Game.matrix[y0 - 2][x0 + 0] = Game.defaultColor;
-  Game.matrix[y0 - 3][x0 + 1] = Game.defaultColor;
-  Game.matrix[y0 - 3][x0 + 2] = Game.defaultColor;
+  constructor() {
+    const counter = document.getElementById(this.name);
+    counter.textContent = this.text + this.value;
+  }
+  count() {
+    const counter = document.getElementById(this.name);
+    counter.textContent = this.text + this.value++;
+  }
+  set(x = 0) {
+    this.value = x;
+    const counter = document.getElementById(this.name);
+    counter.textContent = this.text + this.value;
+  }
 }
+const counter = new Counter();
 
-// Mainloop
-function run() {
-  Game.transformMatrix();
-  viewport_container(Game.matrix);
-  const counter = document.getElementById("counter");
-  counter.textContent = "Rounds: " + Game.rounds;
-}
+// ---------------------------------------------  Buttons ----------------------------------------------------- //
 
-Game.new();
-r_Pentomino();
-let loop = null;
-let started = false;
-viewport_container(Game.matrix); // Erstes Bild zeigen (r-Pentomino)
-
-/*********************************************** Buttons *******************************/
-
-// Start/Stop
+// Start/Stop-Button
 document.getElementById("control").addEventListener("click", () => {
   const butt = document.getElementById("control");
-  if (!started) {
-    loop = setInterval(run, intervall); // <============ Mainloop setzen
-    started = true;
-    butt.textContent = "Stop";
-  } else {
-    started = false;
-    clearInterval(loop);
 
+  if (Simulation.inProgress) {
+    Simulation.stop();
     if (Game.rounds === 0) butt.textContent = "Start";
     else butt.textContent = "Continue";
+  } else {
+    Simulation.start();
+    butt.textContent = "Stop";
   }
 });
 
-// Reset to defaults
+// Resetbutton
 document.getElementById("Reset").addEventListener("click", () => {
-  clearInterval(loop);
-  started = false;
-  Game.new();
-  const counter = document.getElementById("counter");
-  counter.textContent = "Rounds: " + Game.rounds;
+  Simulation.reset();
+  counter.set();
   const butt = document.getElementById("control");
   butt.textContent = "Start";
-  r_Pentomino();
-  viewport_container(Game.matrix);
 });
 
 // Editbutton
 document.getElementById("Edit").addEventListener("click", () => {
-  clearInterval(loop);
-  started = false;
-  const counter = document.getElementById("counter");
-  counter.textContent = "Rounds: " + Game.rounds;
+  Simulation.stop();
   const butt = document.getElementById("control");
   butt.textContent = "Continue";
   viewport_container(Game.matrix);
@@ -231,15 +252,13 @@ document.getElementById("Edit").addEventListener("click", () => {
 
 /*******************************************************************************
 
-Editor funktioniert, aber ist sicher noch ausbaufähig.
-Kleinigkeiten wurden geändert.
+Aufgeräumt.
 
 Fehlt noch:
-- Funktion transformMatrix ineffizient, muss knackiger werden
 - Random-Fill
 - Rulemanagement
 - Was wo man draufklickt und dann was zu lesen kriegt
-- Makeup
+- Paar mehr Knöppe und Makeup
 
 ********************************************************************************/
 
